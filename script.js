79ef4e9e7e0152276b2cd000982f92f1c06ccbab
Katrine Meyer <katrinemeyer@Katrines-MacBook-Pro.local>
2019-12-13 09:15:35 +0100

    tilføjet at man kan klikke

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ window.addEventListener("load", startVises);
 // Hvad skal i skrive for at skjule den div?
 
 function startVises() {
+    document.querySelector("#menuknap").addEventListener("click", clickMenuKnap);
+
+}
+
+function clickMenuKnap() {
 
 }
