426e1d232498605ec342db47809d6b3b8506567e
Katrine Meyer <katrinemeyer@Katrines-MacBook-Pro.local>
2019-12-13 09:00:30 +0100

    hvad starter js med?

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
+//hvad starter js med?
+window.addEventListener("load", startVises);
+
 // Hvad skal i skrive for at skjule den div?
-document.querySelector("#menu").classList.remove("hidden");
+
+function startVises() {
+    document.querySelector("#menu").classList.remove("hidden");
+}
